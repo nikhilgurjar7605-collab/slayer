c3313b9e4aed3fa71f0e351146ac45b813b635dd
Whitedevil20135 <111232663+Whitedevil20135@users.noreply.github.com>
2026-04-09 11:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1608,7 +1608,7 @@ ENEMY_IMAGES = {
     "Vampire":              "",
     "Ghoul":                "",
     "Cursed Spirit":        "",
-    "Rogue Demon":          "AgACAgUAAxkBAAECA5tp10HwkzDRkwbnQ2BqWLsQ7Erz2wAClA1rG_HjwVZnGsCqn7RDIAEAAwIAA3kAAzsE",
+    "Rogue Demon":          "",
 
     # ── Butterfly Estate ─────────────────────────────────────────────────
     "Forest Wraith":        "",
