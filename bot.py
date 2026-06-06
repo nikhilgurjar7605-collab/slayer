c41731266f2f69a4de1c4952754ce5987571e4fe
nikhilgurjar7605-collab <nikhilgurjar7605@gmail.com>
2026-06-06 11:26:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,7 +180,8 @@ from handlers.stockmarket import (
     marketcrash, marketboom, marketreset, update_stock_prices,
     log_stock_event, nudge_price, STOCKS,
     stock_view_callback, stock_buy_callback, stock_sell_callback,
-    stock_sell_confirm_callback, stock_back_callback, market_page_callback
+    stock_sell_confirm_callback, stock_back_callback, market_page_callback,
+    addstock, removestock, nudgeprice_cmd, stock_portfolio_callback,
 )
 from handlers.referral import referral
 from handlers.style_art import breathing, art, givestyle, giveart
@@ -601,6 +602,7 @@ async def callback_router(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif data.startswith('stock_sell_confirm_'): await stock_sell_confirm_callback(update, context)
     elif data.startswith('stock_sell_'):       await stock_sell_callback(update, context)
     elif data == 'stock_back':                 await stock_back_callback(update, context)
+    elif data == 'stock_portfolio':            await stock_portfolio_callback(update, context)
     elif data.startswith('market_page_'):      await market_page_callback(update, context)
     elif data.startswith('myskills_'):          await myskills_callback(update, context)
     elif data == 'goto_start':                await start(update, context)
@@ -875,6 +877,9 @@ def main():
         ('marketcrash',   marketcrash),
         ('marketboom',    marketboom),
         ('marketreset',   marketreset),
+        ('addstock',      addstock),
+        ('removestock',   removestock),
+        ('nudgeprice',    nudgeprice_cmd),
         ('addgifbanner',    addgifbanner),
         ('removegifbanner', removegifbanner),
         ('listgifbanners',  listgifbanners),
